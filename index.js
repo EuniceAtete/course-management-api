e58fd03e5c6ade089b950f1a01589b984879defa
Eunice Atete <euniceatete0@gmail.com>
2026-02-23 20:11:56 +0200

    Using simple GET requests

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(bodyParser.json());
 
-
+app.get('/',(req,res) => {
+    res.send("Welcome to the course management API!")
+})
 
 app.listen(5000,() => {
     console.log("Server running on port 5000...")
